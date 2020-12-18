30d70153b7e19681ed592b0b2ed1ab5b3062022c
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2020-12-18 21:10:29 +0530

    added testing method for JSON code

--- src/Json.js
+++ src/Json.js
@@ -7,12 +7,19 @@ import {
   setupUMLDiagram,
   diagram,
 } from './gojsHelper';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Input, Button, Message } from 'semantic-ui-react';
 import Highlight from 'react-highlight.js';
+import { ModelManager } from '@accordproject/concerto-core';
+import CodeEditor from './Editor';
 
 const Json = () => {
   const [model, updateModel] = useState([]);
   const [code, updateCode] = useState('');
+  const [modelName, updateModelName] = useState('model');
+  const [testResult, toggleTestResult] = useState('');
+  const [editMode, toggleEditMode] = useState(false);
+  const [visibleMessage, toggleVisibleMessage] = useState(true);
+
   const addModelListener = () => {
     diagram.addModelChangedListener(evt => {
       let newModel = evt.model.Cc;
@@ -37,9 +44,10 @@ const Json = () => {
       }
       return newObj;
     })
-    let finalModel = { model: [] };
-    finalModel.model = newModel;
-    let jsonCode = JSON.stringify(finalModel, null, 2);
+    let finalModel = {};
+    finalModel[modelName] = [];
+    finalModel[modelName] = newModel;
+    let jsonCode = JSON.stringify(finalModel, null, 2)
     updateCode(jsonCode);
   }
 
@@ -53,20 +61,56 @@ const Json = () => {
     addModelListener()
   }
 
+  const handleClick = () => {
+    if (!visibleMessage) toggleVisibleMessage(true);
+    try {
+      if (JSON.stringify(JSON.parse(code), null, 2) == code)
+        toggleTestResult('Valid JSON (RFC 8259)');
+      else toggleTestResult('Invalid JSON (RFC 8259)');
+    } catch (err) {
+      toggleTestResult('Invalid JSON (RFC 8259). Error parsing JSON.')
+    }
+  }
+
+  const handleEditCodeClick = () => {
+    toggleEditMode(!editMode);
+  }
+
+  const onCodeModify = code => {
+    updateCode(code);
+  }
+
+  const handleDismiss = () => {
+    toggleVisibleMessage(false);
+  }
+
   useEffect(() => {
     setupDiagram();
     setupPalette();
-  }, [model])
+  }, [model]);
 
   return (
     <React.Fragment>
       <Grid columns={2}>
         <Grid.Column width={7}>
-          <Highlight language={'json'}>
+          <Button primary onClick={handleEditCodeClick}>
+            Edit Code
+          </Button>
+          {!editMode && <Highlight language={'json'}>
             {code}
-          </Highlight>
+          </Highlight>}
+          {editMode && <CodeEditor code={code} onChange={onCodeModify} />}
         </Grid.Column>
         <Grid.Column width={8}>
+          <div className="Json-tester">
+            <Button primary onClick={handleClick}>
+              Test JSON Code
+            </Button>
+            {(testResult.length > 0 && visibleMessage) &&
+              <Message onDismiss={handleDismiss}>
+                {testResult}
+              </Message>}
+          </div>
           <Diagram
             model={model}
             paletteClass='myPalette'
